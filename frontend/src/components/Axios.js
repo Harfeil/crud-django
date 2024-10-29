9c246d5ac1bcd05b9d68261ea385e5020f5e3dcb
Harfeil <125838282+Harfeil@users.noreply.github.com>
2024-10-29 12:37:35 -0700

    updated

--- frontend/src/components/Axios.js
+++ frontend/src/components/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8001/";
+const baseURL = "http://tg808400ow0wgc4owsoo8gkc.66.42.59.139.sslip.io/";
 const AxiosInstance = axios.create({
   baseURL: baseURL,
   timeout: 10000,
